19f754945f2b559b798d2ca1b32d30b226c5982c
Yhan Carlos <yhan.trujillo@correounivalle.edu.co>
2024-10-30 14:14:08 -0500

    Eliminado Nombre Pokemon

--- app/src/main/java/com/example/dadm/view/MainActivity.kt
+++ app/src/main/java/com/example/dadm/view/MainActivity.kt
@@ -9,9 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
     }
 }
