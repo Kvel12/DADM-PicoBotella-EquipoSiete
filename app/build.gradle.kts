7a53794d2798ddf9ee475638bfd25ff3156456d4
Yhan Carlos <yhan.trujillo@correounivalle.edu.co>
2024-11-27 15:34:24 -0500

    tests working correctly
    Hay 12 pruebas de 13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
-    id ("kotlin-kapt")
-    id ("com.google.dagger.hilt.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -19,7 +19,6 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
 
     buildTypes {
         release {
@@ -44,62 +43,74 @@ android {
 }
 
 dependencies {
-    val navVersion = "2.8.3"
+    // Core de Android
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.arch.core:core-testing:2.2.0")
 
-    //testing
-    testImplementation("junit:junit:4.13.2")
+    // JUnit 5 para pruebas unitarias
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
+
+    // Mockito para simulaciones
     testImplementation("org.mockito:mockito-core:5.14.2")
     testImplementation("org.mockito:mockito-inline:5.2.0")
-    testImplementation ("org.mockito:mockito-android:5.14.2")
-    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
-    testImplementation ("androidx.arch.core:core-testing:2.2.0")
-    debugImplementation ("org.jacoco:org.jacoco.core:0.8.12")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
+    testImplementation("org.mockito:mockito-android:5.14.2")
+    testImplementation("org.robolectric:robolectric:4.10")
+
+
+    // Testing con LiveData y coroutines
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+
+    // Pruebas instrumentadas (opcional)
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
 
-    //navigation
-    implementation ("androidx.navigation:navigation-fragment-ktx:$navVersion")
-    implementation ("androidx.navigation:navigation-ui-ktx:$navVersion")
+    // Navigation
+    val navVersion = "2.8.3"
+    //noinspection GradleDependency
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    //noinspection GradleDependency
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+    //noinspection GradleDependency
     implementation("androidx.navigation:navigation-common:$navVersion")
 
-    //cardView
+    // CardView y RecyclerView
     implementation("androidx.cardview:cardview:1.0.0")
-    //RecyclerView
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 
-    //corrutinas
+    // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
 
-    //viewmodel
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-    implementation ("androidx.activity:activity-ktx:1.9.3")
-    implementation ("androidx.fragment:fragment-ktx:1.8.5")
-
-    // LiveData
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    // ViewModel y LiveData
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
 
     // Room
-    implementation ("androidx.room:room-runtime:2.6.1")
-    implementation ("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
-    implementation ("com.getbase:floatingactionbutton:1.10.1")
+    implementation("com.getbase:floatingactionbutton:1.10.1")
 
-    //Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 
-    //Glide
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 
-    //Animaciones
-    implementation ("com.airbnb.android:lottie:6.6.0")
+    // Animaciones
+    implementation("com.airbnb.android:lottie:6.6.0")
 
-    //dagger hilt
+    // Dagger Hilt
     implementation("com.google.dagger:hilt-android:2.47")
     kapt("com.google.dagger:hilt-android-compiler:2.47")
 }
-
